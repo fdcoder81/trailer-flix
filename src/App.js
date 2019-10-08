974204a91cf18ed774f8d6113c53c4d019e74754
Fdcoder81 <carloanselmi81@gmail.com>
2019-10-08 17:40:28 +0200

    Added slider and arrow logic

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import MovieItem from "./MovieItem";
 import { getMoviesNow, getTvSeries, getMoviesByGenre } from "./api/getData";
 
 class App extends React.Component {
-  state = { moviesNow: [], tvSeries: [], actionMovies: [], animation: [] };
+  state = {
+    moviesNow: [],
+    tvSeries: [],
+    actionMovies: [],
+    animation: [],
+    translate: 0
+  };
 
-  containerSection = React.createRef();
+  moviesNowSection = React.createRef();
+  tvSeriesSection = React.createRef();
+  actionMovieSection = React.createRef();
+  animationSection = React.createRef();
 
   componentDidMount = async () => {
     const moviesNow = await getMoviesNow();
@@ -23,41 +32,113 @@ class App extends React.Component {
     this.setState({ animation });
   };
 
-  handleClick = e => {
-    console.log(this.containerSection.current.style);
-    this.containerSection.current.style.transform = `translateX(-100%)`;
+  handleNext = ref => e => {
+    if (ref.current.style.transform === "") {
+      this.setState({ translate: 0 }, () => {
+        ref.current.style.transform = `translateX(${this.state.translate}%)`;
+      });
+    }
+
+    if (!ref.current.style.transform.includes("300")) {
+      this.setState(
+        prevState => ({ translate: prevState.translate - 100 }),
+        () => {
+          ref.current.style.transform = `translateX(${this.state.translate}%)`;
+        }
+      );
+    }
+  };
+
+  handlePrev = ref => e => {
+    if (this.state.translate !== 0) {
+      this.setState(
+        prevState => ({ translate: prevState.translate + 100 }),
+        () => {
+          ref.current.style.transform = `translateX(${this.state.translate}%)`;
+        }
+      );
+    }
   };
 
   render() {
     return (
       <div className="container">
         <h1>Trailer Flix</h1>
-        <button onClick={this.handleClick}>CLICK</button>
 
         <h1 className="section-title">Now playing</h1>
-        <div ref={this.containerSection} className="container-section">
-          <div className="prevArrow">
+        <div className="wrapper">
+          <div
+            className="prevArrow"
+            onClick={this.handlePrev(this.moviesNowSection)}
+          >
             <img src={prevArrow} alt="" />
           </div>
-          <div className="nextArrow">
+          <div
+            className="nextArrow"
+            onClick={this.handleNext(this.moviesNowSection)}
+          >
             <img src={nextArrow} alt="" />
           </div>
-          <MovieItem movies={this.state.moviesNow} />
+          <div ref={this.moviesNowSection} className="container-section">
+            <MovieItem movies={this.state.moviesNow} />
+          </div>
         </div>
 
         <h1 className="section-title">TV Series</h1>
-        <div className="container-section">
-          <MovieItem movies={this.state.tvSeries} />
+        <div className="wrapper">
+          <div
+            className="prevArrow"
+            onClick={this.handlePrev(this.tvSeriesSection)}
+          >
+            <img src={prevArrow} alt="" />
+          </div>
+          <div
+            className="nextArrow"
+            onClick={this.handleNext(this.tvSeriesSection)}
+          >
+            <img src={nextArrow} alt="" />
+          </div>
+          <div ref={this.tvSeriesSection} className="container-section">
+            <MovieItem movies={this.state.tvSeries} />
+          </div>
         </div>
 
-        <h1 className="section-title">Children & Family</h1>
-        <div className="container-section">
-          <MovieItem movies={this.state.animation} />
+        <div className="wrapper">
+          <div
+            className="prevArrow"
+            onClick={this.handlePrev(this.animationSection)}
+          >
+            <img src={prevArrow} alt="" />
+          </div>
+          <div
+            className="nextArrow"
+            onClick={this.handleNext(this.animationSection)}
+          >
+            <img src={nextArrow} alt="" />
+          </div>
+          <h1 className="section-title">Children & Family</h1>
+          <div ref={this.animationSection} className="container-section">
+            <MovieItem movies={this.state.animation} />
+          </div>
         </div>
 
-        <h1 className="section-title">Action Movies</h1>
-        <div className="container-section">
-          <MovieItem movies={this.state.actionMovies} />
+        <div className="wrapper">
+          <div
+            className="prevArrow"
+            onClick={this.handlePrev(this.actionMovieSection)}
+          >
+            <img src={prevArrow} alt="" />
+          </div>
+          <div
+            className="nextArrow"
+            onClick={this.handleNext(this.actionMovieSection)}
+          >
+            <img src={nextArrow} alt="" />
+          </div>
+          <h1 className="section-title">Action Movies</h1>
+          <div ref={this.actionMovieSection} className="container-section">
+            <MovieItem movies={this.state.actionMovies} />
+          </div>
         </div>
       </div>
     );
