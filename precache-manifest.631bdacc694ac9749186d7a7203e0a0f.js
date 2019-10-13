700c3c32e28f15553cbfefa1887f3d5653e79480
Fdcoder81 <carlooanselmi81@gmail.com>
2019-10-13 09:17:32 +0200

    Updates

---
+++ precache-manifest.631bdacc694ac9749186d7a7203e0a0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e57cf2eb841958dd30695a6324260b",
+    "url": "/trailer-flix/index.html"
+  },
+  {
+    "revision": "b05877634df75f9137ed",
+    "url": "/trailer-flix/static/css/main.5206982d.chunk.css"
+  },
+  {
+    "revision": "063165568b3d6b57b213",
+    "url": "/trailer-flix/static/js/2.98a90f3f.chunk.js"
+  },
+  {
+    "revision": "b05877634df75f9137ed",
+    "url": "/trailer-flix/static/js/main.22af39a5.chunk.js"
+  },
+  {
+    "revision": "b89c929cfb7f3a95d72f",
+    "url": "/trailer-flix/static/js/runtime-main.b6f33b6a.js"
+  }
+]);
